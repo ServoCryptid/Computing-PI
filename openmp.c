34d0633491efc4e473773a6f8af383104451691f
larisa.biriescu <larisa.biriescu@Everseen.global>
2020-07-26 22:17:11 +0300

    using rand_r for generation of the random numbers

--- openmp.c
+++ openmp.c
@@ -1,74 +1,57 @@
-// the code for generating PI using monte carlo
-#include <omp.h>
+/* Program to compute Pi using Monte Carlo methods */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
 #include <time.h>
-#include <math.h>
-#define SEED 35791246
+#include <sys/time.h>
+#include <omp.h>
 
-int points_generation_rand(long int niter){ //the first attempt, rand() is not thread safe!
-	int j, count=0;
-	double x,y;
+#define FPTYPE double //TODO: why?
 
-   /* initialize random numbers */
-   srand(SEED);
-
-	for (j=0; j<niter; j++){
-		x = (double)rand()/RAND_MAX; //rand() is not thread safe; try different implementations
-		y = (double)rand()/RAND_MAX;
-		if (x*x+y*y<1) count++; 
-	}
-
-	return count;
+static double rtclock(){
+   struct timeval Tp;
+   gettimeofday (&Tp, NULL);
+   return (Tp.tv_sec + Tp.tv_usec * 1.0e-6);
 }
 
+int main(int argc, char* argv[])
+{  
 
-int points_generation_drand48_r(long int niter){
-	int j, count=0;
-	struct drand48_data buffer;
-	double x,y;
-
-   /* initialize random numbers */
-   srand48_r(SEED, &buffer);
-
-	for (j=0; j<niter; j++){
-		drand48_r(&buffer, &x);
-		drand48_r(&buffer, &y);
-		if (x*x+y*y<1) count++; 
-	}
-
-	return count;
-}
-
-
-main(int argc, char* argv[])
-{
-   long int ndata_points=0; 
+   char *value = argv[1], *value2 = argv[2];    //first arg is the # of points, second arg is the #of threads
    int nthreads;
-   long int niter;
-   long int i, j, count=0; /* # of points in the 1st quadrant of unit circle */
-   double pi;
-   char *value = argv[1]; 	//first arg is the # of points
-   
-   clock_t begin = clock(); //is it ok to start timer here?
+   long long int niter, ndata_points;
+   long long int i,count=0; 
+   FPTYPE pi;
+   double startt, stopt;
 
-   ndata_points = atoi(value);
-
-   nthreads = omp_get_max_threads();
+   ndata_points = atoll(value);
+   nthreads = atoi(value2);
 
    niter = ndata_points/nthreads;
 
-   #pragma omp parallel for shared(niter) reduction(+:count)
-   for (i=0; i<nthreads; i++) { 
-   		count += points_generation_drand48_r(niter);
-    }
+   startt = rtclock();
 
-   	pi= 4 * (double)count/ndata_points;
+   /* initialize random numbers */
+   count=0;
 
-   	printf("# of trials= %d , estimate of pi is %g, exec. time %g seconds \n",
-   			niter,pi,(double)(clock() - begin)/CLOCKS_PER_SEC);
+#pragma omp parallel
+   {
+      unsigned int seed = ((time(NULL)) ^ omp_get_thread_num());
 
-   	return 0;
-}	
+#pragma omp for private(i) reduction(+:count) schedule(dynamic) 
+      for ( i=0; i<niter; i++) {
+         FPTYPE x = (FPTYPE)rand_r(&seed)/RAND_MAX;
+         FPTYPE y = (FPTYPE)rand_r(&seed)/RAND_MAX;
+         if (x*x+y*y<=1) count++;
+      }
+   }
+   pi=4*(FPTYPE)count/niter;
+
+   stopt = rtclock();
+   
+   printf("time:%g,pi:%g \n", stopt-startt, pi);
+
+   return 0;
+}
